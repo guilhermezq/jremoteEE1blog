53f6831271f78d039f3b4dbb1215679a2b556ab2
Guilherme Quirino <guilhermezq@gmail.com>
2021-02-07 09:23:36 +0200

    first commit

---
+++ src/main/java/ee/sda/jremoteEE1blog/JremoteEe1blogApplication.java
@@ -0,0 +1,13 @@
+package ee.sda.jremoteEE1blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JremoteEe1blogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JremoteEe1blogApplication.class, args);
+	}
+
+}
